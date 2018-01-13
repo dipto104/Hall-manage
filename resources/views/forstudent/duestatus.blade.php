fd7a96d1ce91224054d3d5887045ff0b90d934f4
Dipto Roy <diptoroy.roy1912@gmail.com>
2018-01-13 09:00:24 +0600

    67th comm : duestatus modified

--- resources/views/forstudent/duestatus.blade.php
+++ resources/views/forstudent/duestatus.blade.php
@@ -3,15 +3,27 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h1 class="col-md-4 col-md-offset-1">
+            <div class="col-md-4 col-md-offset-1">
+                <h1>
+                    Due Status
+                 </h1>
+            </div>
 
-                    <h1>Due Status</h1>
-                </div>
         </div>
         <div class="row">
             @foreach($data as $datum)
                 @if(!($datum->due==0))
-                    <div class="col-md-6 col-md-offset-1">
+                    <div class="col-lg-4 mb-4">
+                        <div class="card h-100">
+                            <h4 class="card-header bg-info text-white">Term No : {{$datum->termno}}</h4>
+                            <div class="card-body">
+                                <p class="lead">Total Mess : {{$datum->totalmess}}</p>
+                                <p class="lead">Due : {{$datum->due}} /-taka</p>
+                                <p class="lead">Remarks : {{$datum->remarks}}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--<div class="col-md-6 col-md-offset-1">
                         <div class="panel panel-default">
                         <div class="panel-heading">Term No : {{$datum->termno}}</div>
 
@@ -21,7 +33,7 @@
                                     <p class="lead">Remarks : {{$datum->remarks}}</p>
                                 </div>
                         </div>
-                    </div>
+                    </div>-->
                 @endif
             @endforeach
         </div>
